24c3542f5b017b19672baadf420952139bec9390
Andrei Timar <atimar999@gmail.com>
2021-09-09 21:17:38 +0300

    Added Demo Project

---
+++ PluginDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JiraScriptBeePlugin.Loaders;
+
+namespace PluginDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage <path_to_json>");
+                return;
+            }
+
+            var modelLoader = new ModelLoader();
+            var loadedModels = modelLoader.LoadModel(new List<string>
+            {
+                File.ReadAllText(args[0])
+            });
+        }
+    }
+}
