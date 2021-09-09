6908d4744953d11b683da70685267710e8581def
Andrei Timar <atimar999@gmail.com>
2021-09-09 21:18:07 +0300

    Added Loader

---
+++ JiraScriptBeePlugin/Loaders/ModelLoader.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using JiraScriptBeePlugin.Models;
+using Newtonsoft.Json;
+using ScriptBeePlugin;
+using Serilog;
+
+namespace JiraScriptBeePlugin.Loaders
+{
+    public class ModelLoader : IModelLoader
+    {
+        private readonly StaticToReferenceModelConverter _modelConverter =
+            new(new LoggerConfiguration().WriteTo.File("logs.txt").CreateLogger());
+
+        public Dictionary<string, Dictionary<string, ScriptBeeModel>> LoadModel(List<string> fileContents,
+            Dictionary<string, object> configuration = null)
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                Converters = new List<JsonConverter>
+                {
+                    new NullToDefaultValueConverter<long>()
+                }
+            };
+
+            var dictionary = new Dictionary<string, Dictionary<string, ScriptBeeModel>>();
+
+            var issuesDictionary = new Dictionary<string, ScriptBeeModel>();
+            var authorsDictionary = new Dictionary<string, ScriptBeeModel>();
+            
+            foreach (var content in fileContents)
+            {
+                var projectResult = JsonConvert.DeserializeObject<StaticProjectResult>(content, jsonSerializerSettings);
+
+                var convertedModel = _modelConverter.Convert(projectResult);
+
+                foreach (var issue in convertedModel.issues)
+                {
+                    issuesDictionary.Add(issue.key, issue);
+                }
+
+                foreach (var user in convertedModel.users)
+                {
+                    authorsDictionary.Add(user.username, user);
+                }
+            }
+
+            dictionary.Add("Issue", issuesDictionary);
+            dictionary.Add("Author", authorsDictionary);
+
+            return dictionary;
+        }
+
+        public string GetName()
+        {
+            return "jira";
+        }
+    }
+}
---
+++ JiraScriptBeePlugin/Loaders/NullToDefaultValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JiraScriptBeePlugin.Loaders
+{
+    public class NullToDefaultValueConverter<TType> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return default(TType);
+            }
+
+            return serializer.Deserialize(reader);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(TType);
+        }
+    }
+}
